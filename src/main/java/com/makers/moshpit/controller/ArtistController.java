92db3d838aad201f6c9f0d1efa44a2dc94e6cf3b
Dylan <your email>
2025-12-16 16:15:11 +0000

    Adding the artist controller

---
+++ src/main/java/com/makers/moshpit/controller/ArtistController.java
@@ -0,0 +1,34 @@
+package com.makers.moshpit.controller;
+
+import com.makers.moshpit.model.Artist;
+import com.makers.moshpit.model.Post;
+import com.makers.moshpit.repository.ArtistRepository;
+import com.makers.moshpit.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class ArtistController {
+
+    @Autowired
+    private ArtistRepository artistRepository;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @GetMapping("/artists/{id}")
+    public String getArtist(@PathVariable Long id, Model model) {
+        Artist artist =  artistRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Artist not found"));
+        Iterable<Post> posts = postRepository.findAll();
+
+        model.addAttribute("posts", posts);
+        model.addAttribute("artist", artist);
+        return "artist_page";
+    }
+
+
+}

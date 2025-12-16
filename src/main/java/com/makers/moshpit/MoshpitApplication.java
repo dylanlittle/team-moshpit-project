8f53503c361f133ad8973cb7b6f26fca05783c9c
Dylan <your email>
2025-12-16 10:50:13 +0000

    Initial project files

---
+++ src/main/java/com/makers/moshpit/MoshpitApplication.java
@@ -0,0 +1,13 @@
+package com.makers.moshpit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoshpitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoshpitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/makers/moshpit/MoshpitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makers.moshpit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoshpitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

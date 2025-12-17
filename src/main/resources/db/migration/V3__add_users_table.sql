e2b473f1c26f9fa1496c8a1e3fc3de00602fba1c
Dylan <your email>
2025-12-17 15:08:48 +0000

    updated table, so only the email has to be unique

--- src/main/resources/db/migration/V3__add_users_table.sql
+++ src/main/resources/db/migration/V3__add_users_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
                        id bigserial PRIMARY KEY,
-                       name VARCHAR(255) NOT NULL UNIQUE,
+                       name VARCHAR(255) NOT NULL,
                        email VARCHAR(255) NOT NULL UNIQUE,
                        bio VARCHAR(500),
                        active_artist_id BIGINT

fae77ac74b154ccccbf20c774e5e23ed53ba6ddc
Dylan <your email>
2025-12-17 11:45:35 +0000

    Post updates appear, and bio populates left hand side box

--- src/main/java/com/makers/moshpit/repository/PostRepository.java
+++ src/main/java/com/makers/moshpit/repository/PostRepository.java
@@ -1,7 +1,11 @@
 package com.makers.moshpit.repository;
 
 import com.makers.moshpit.model.Post;
+import org.springframework.data.domain.Limit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findAllByArtistIdOrderByTimestampDesc(Long artistId);
 }

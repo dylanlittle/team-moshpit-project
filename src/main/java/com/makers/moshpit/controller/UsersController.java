798de00ca2fa26ac43784105949fd19d25a13a45
Eloise Weeks <eloiseweeks1@gmail.com>
2025-12-18 15:07:03 +0000

    Initial commit
    # Conflicts:
    #	src/main/java/com/makers/moshpit/controller/UsersController.java

--- src/main/java/com/makers/moshpit/controller/UsersController.java
+++ src/main/java/com/makers/moshpit/controller/UsersController.java
@@ -5,13 +5,16 @@ import com.makers.moshpit.model.User;
 import com.makers.moshpit.repository.PostRepository;
 import com.makers.moshpit.repository.UserRepository;
 import com.makers.moshpit.service.AuthService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class UsersController {
@@ -32,7 +35,7 @@ public class UsersController {
         User user = userRepository.findUserByEmail(email)
                 .orElseGet(() -> userRepository.save(new User(email)));
 
-        if (user.getUsername() == null || user.getUsername().isEmpty()) {
+        if (user.getUsername() == null || user.getUsername().equals("null") || user.getUsername().isEmpty()) {
             return "redirect:/users/create";
         }
         return "redirect:/user";
@@ -40,16 +43,25 @@ public class UsersController {
 
     @GetMapping("/users/create")
     public String getCreateProfileForm(Model model) {
-        model.addAttribute("currentUser", authService.getCurrentUser());
+        if (!model.containsAttribute("newUser")) {
+            model.addAttribute("newUser", authService.getCurrentUser());
+        }
         return "/users/create_profile";
     }
 
     @PostMapping("/users/create")
-    public String submitCreateProfileForm(@ModelAttribute User newUser) {
+    public String submitCreateProfileForm(@Valid @ModelAttribute User newUser, BindingResult result, RedirectAttributes redirectAttributes) {
+
+        if (result.hasErrors()) {
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.newUser", result);
+            redirectAttributes.addFlashAttribute("newUser", newUser);
+            return "redirect:/users/create";
+        }
         User currentUser = authService.getCurrentUser();
         currentUser.setUsername(newUser.getUsername());
         currentUser.setName(newUser.getName());
         currentUser.setBio(newUser.getBio());
+        currentUser.setLocation(newUser.getLocation());
         userRepository.save(currentUser);
         return "redirect:/user";
     }
--- src/main/java/com/makers/moshpit/model/User.java
+++ src/main/java/com/makers/moshpit/model/User.java
@@ -1,6 +1,7 @@
 package com.makers.moshpit.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -16,17 +17,23 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Please provide a valid name.")
     private String name;
+    @NotBlank(message = "Please provide a valid username.")
     private String username;
     private String email;
     private String bio;
     private Long active_artist_id;
+    @NotBlank(message = "Please provide a valid location. For example: London, UK")
+    private String location;
 
     public User(String email) {
         this.name = "";
         this.email = email;
         this.username = "";
         this.bio = "";
+        this.location = "";
         this.active_artist_id = null;
     }
 }

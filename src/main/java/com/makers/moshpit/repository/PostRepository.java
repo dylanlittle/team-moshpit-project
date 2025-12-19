0b4f5c32439cc03a692e418a3644c76dd98e5fe4
Ollie Jeffcote <oliverjeffcote@gmail.com>
2025-12-19 17:04:19 +0000

    merging with main

--- src/main/java/com/makers/moshpit/repository/PostRepository.java
+++ src/main/java/com/makers/moshpit/repository/PostRepository.java
@@ -1,8 +1,11 @@
 package com.makers.moshpit.repository;
 
 import com.makers.moshpit.model.Post;
+import com.makers.moshpit.model.User;
 import org.springframework.data.domain.Limit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,4 +13,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findAllByArtistIdOrderByTimestampDesc(Long artistId);
     Iterable<Post> findAllByUserIdOrderByTimestampDesc(Long userId);
     List<Post> findAllByConcertIdOrderByTimestampDesc(Long concertId);
+
+    @Query("SELECT p FROM Post p JOIN Follow f ON p.artist = f.artist WHERE f.user = :user ORDER BY p.timestamp DESC")
+    List<Post> findPostsByFollowedArtists(@Param("user") User user);
 }

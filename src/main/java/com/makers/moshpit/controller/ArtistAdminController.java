c5393c36f310d184d245594be2f2f6d544c24205
Ollie Jeffcote <oliverjeffcote@gmail.com>
2026-01-06 14:33:11 +0000

    draft pr for admin service

--- src/main/java/com/makers/moshpit/controller/ArtistAdminController.java
+++ src/main/java/com/makers/moshpit/controller/ArtistAdminController.java
@@ -4,6 +4,7 @@ import com.makers.moshpit.model.User;
 import com.makers.moshpit.repository.ArtistAdminRepository;
 import com.makers.moshpit.repository.UserRepository;
 import com.makers.moshpit.service.AuthService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -32,13 +33,19 @@ public class ArtistAdminController {
         return "redirect:/artists/" + id;
     }
 
-    @PostMapping("/artists/{id}/switch-to-user")
-    public String userMode(@PathVariable Long id, Model model) {
+    @PostMapping("/switch-to-user")
+    public String userMode(Model model, HttpServletRequest request) {
         User user = authService.getCurrentUser();
 
         user.setActive_artist_id(null);
         userRepository.save(user);
 
-        return "artist_page";
+        String referer = request.getHeader("Referer");
+
+        if (referer == null || referer.isEmpty()) {
+            return "redirect:/";
+        }
+
+        return "redirect:" + referer;
     }
 }

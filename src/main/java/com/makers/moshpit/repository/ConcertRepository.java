e4ca821f85d06f2f142f18e201e64cb149aa73bd
Dylan <your email>
2026-01-08 11:13:36 +0000

    Home now only displays upcoming concerts for followed artists

--- src/main/java/com/makers/moshpit/repository/ConcertRepository.java
+++ src/main/java/com/makers/moshpit/repository/ConcertRepository.java
@@ -2,7 +2,10 @@ package com.makers.moshpit.repository;
 
 import com.makers.moshpit.model.Concert;
 import com.makers.moshpit.model.Post;
+import com.makers.moshpit.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.Date;
@@ -13,4 +16,7 @@ public interface ConcertRepository extends JpaRepository<Concert, Long> {
     List<Concert> findAllByArtistIdAndConcertDateAfterOrderByConcertDateAsc(Long artistId, LocalDate dateToday);
     List<Concert> findAllByArtistIdAndConcertDateBeforeOrderByConcertDateDesc(Long artistId, LocalDate dateToday);
     List<Concert> findByConcertDateAfterOrderByConcertDateAsc(LocalDate dateToday);
+
+    @Query("SELECT c FROM Concert c JOIN Follow f ON c.artist = f.artist WHERE f.user = :user")
+    List<Concert> findConcertsByFollowedArtists(@Param("user") User user);
 }
